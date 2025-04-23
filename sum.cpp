d8e626e964e8320a3f85d71f0ffabd98014025ac
krishnabrahmajosyula <krishnabrahmajosyula@gmail.com>
2025-04-23 23:45:16 +0530

    feat: Print message after printing array elements

--- sum.cpp
+++ sum.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-// small change
 int main(void)
 {
     int arr[10]={0};
     for(int i=0;i<10;i++){
         cout<<arr[i]<<" ";
     }
+    cout<<"Printing the elements is done"<<endl;
     cout<<endl;
     return 0;
 }
